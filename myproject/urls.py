c45468031e5b3e0b1f9e3f2af4c9986a36cf5661
Ripudaman Singh <rsingh_bemba16@thapar.edu>
2019-03-04 13:00:19 +0530

    msg

--- myproject/urls.py
+++ myproject/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     url(r'^contact/', include('contact.urls', namespace='contact')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'', include('core.urls', namespace='core')),
-    url(r'^unipoles/', include('cities.urls', namespace='cities')),
+    url(r'^cities/', include('cities.urls', namespace='cities')),
 ]
 
 if settings.DEBUG:
